0663488e9412dd1c7003c709012ba87938d6a255
Pietromaria <76210679+Pietromaria@users.noreply.github.com>
2020-12-24 16:19:44 +0100

    Update impiccatofunzioni.cpp

--- impiccatofunzioni.cpp
+++ impiccatofunzioni.cpp
@@ -6,3 +6,70 @@
 using namespace std;
 
 
+void personaggio(int t){
+	
+ if (t==6){//riamngono 6 tentativi   primo pezzo
+	cout << "     =======" << endl;
+	cout << "     ||    |" << endl; 
+	cout << "     ||     " << endl;
+	cout << "     ||     "<< endl;
+	cout << "     ||     "<< endl;
+	cout << "   / ||     "<< endl;
+	cout << "  ============= " << endl;
+ }
+ if (t==5){//riamngono cinque tentativi		secondo pezzo
+	cout << "//     =======" << endl;
+	cout << "//     ||    |" << endl; 
+	cout << "//     ||    0" << endl;
+	cout << "//     ||    "<< endl;
+	cout << "//     ||    "<< endl;
+	cout << "//   / ||    "<< endl;
+	cout << "//   ============= " << endl;
+ }
+ if (t==4){//riamngono quattro tentativi		terzo pezzo
+	cout << "     =======" << endl;
+	cout << "     ||    |" << endl; 
+	cout << "     ||    0" << endl;
+	cout << "     ||    |"<< endl;
+	cout << "     ||    "<< endl;
+	cout << "   / ||    "<< endl;
+	cout << "  ============= " << endl;
+ }
+ if (t==3){ //riamngono tre tentativi		quarto pezzo
+	cout << "     =======" << endl;
+	cout << "     ||    |" << endl; 
+	cout << "     ||    0" << endl;
+	cout << "     ||   /|"<< endl;
+	cout << "     ||    "<< endl;
+	cout << "   / ||    "<< endl;
+	cout << "  ============= " << endl;
+ }
+ if (t==2){//riamngono due tentativi	quinto pezzo
+	cout << "    =======" << endl;
+	cout << "    ||    |" << endl; 
+	cout << "    ||    0" << endl;
+	cout << "    ||   /|\\"<< endl;
+	cout << "    ||    "<< endl;
+	cout << "  / ||    "<< endl;
+	cout << "  ============= " << endl;
+ }
+ if (t==1){ // rimane un tentativo		sesto pezzo 
+	cout << "     =======" << endl;
+	cout << "     ||    |" << endl; 
+	cout << "     ||    0" << endl;
+	cout << "     ||   /|\\"<< endl;
+	cout << "     ||   /"<< endl;
+	cout << "   / ||    "<< endl;
+	cout << "  ============= " << endl;
+ }
+ if (t==0){   //tentativi conclusi fine gioco		settimo pezzo
+	cout << "     =======" << endl;
+	cout << "     ||    |" << endl; 
+	cout << "     ||    0" << endl;
+	cout << "     ||   /|\\"<< endl;
+	cout << "     ||   / \\"<< endl;
+	cout << "  /  ||    "<< endl;
+	cout << "  ============= " << endl;
+ }
+
+}
