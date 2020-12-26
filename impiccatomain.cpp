c69bdcc3137a0d362c73fc1ceed5cdc057771d59
matteodoncaf <76475698+matteodoncaf@users.noreply.github.com>
2020-12-26 16:46:57 +0100

    Update impiccatomain.cpp

--- impiccatomain.cpp
+++ impiccatomain.cpp
@@ -7,5 +7,12 @@
 using namespace std;
 
 int main(void){
-	
+	ifstream vocFile;
+	vocFile.open("vocitaliano.txt", ios::in);
+	vocFile.getline()
+	while(!vocFile.eof()){
+		cout<<"Parola: "<<i++<<endl;
+		estraiParola();
+	}
+	vocFile.close()
 }
